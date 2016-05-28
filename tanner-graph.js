269071cf2361f386f6fa9f173a44a679d1fb460b
uzum <a.uzumcuglu@gmail.com>
2016-05-28 17:24:48 +0300

    tanner graph construction by matrix and sub-graph rendering

--- tanner-graph.js
+++ tanner-graph.js
@@ -1,42 +1,82 @@
 // author: anil.uzumcuoglu
 
-function TannerGraph(parameters){
-  this.symbolNodes = parameters.symbolNodes;
-  this.checkNodes = parameters.checkNodes
-  this.symbolNodes.forEach(node => node.group = "symbol");
-  this.checkNodes.forEach(node => node.group = "check");
-  
-  this.edges = parameters.checkNodes.reduce(function(edges, checkNode){
-    var checkNodeEdges = checkNode.neighbors.map(function(symbol){
-      // fill symbolNodes' neighbors lists
-      var targetSymbol = parameters.symbolNodes.filter(s => s.id === symbol)[0];
-      targetSymbol.neighbors.push(checkNode.id);
-      
-      return {
-        from: checkNode.id,
-        to: symbol
+function TannerGraph(matrix){
+  this.matrix = matrix;
+  this.checkNodes = matrix.map( (_, index) => {
+    return {
+      id: index,
+      label: 'S' + index,
+      connections: []
+    };
+  });
+  this.symbolNodes = matrix[0].map( (_, index) => {
+    return {
+      id: index + matrix.length,
+      label: 'C' + index,
+      connections: []
+    };
+  });
+  this.edges = [];
+
+  for(var i = 0; i < matrix.length; i++){
+    for(var j = 0; j < matrix[i].length; j++){
+      if(matrix[i][j] === 1){
+        this.checkNodes[i].connections.push(this.symbolNodes[j]);
+        this.symbolNodes[j].connections.push(this.checkNodes[i]);
+        this.edges.push({ from: this.checkNodes[i].id, to: this.symbolNodes[j].id });
       }
-    });
-    return edges.concat(checkNodeEdges);
-  }, []);
+    }
+  }
 }
 
 TannerGraph.prototype.render = function(){
   if(!TannerGraph.vis) throw new Error("You need to define vis.js first");
-  
+
   var container = document.querySelector('#tanner-graph');
-  
+
   var data = {
-    nodes: new TannerGraph.vis.DataSet(this.symbolNodes.concat(this.checkNodes)),
+    nodes: new TannerGraph.vis.DataSet(this.symbolNodes.map((symbolNode, index) => {
+      symbolNode.group = 'symbol';
+      symbolNode.physics = false;
+      symbolNode.x = index * TannerGraph.width / ( this.symbolNodes.length + 1 );
+      symbolNode.y = TannerGraph.height * 0.25;
+      return symbolNode;
+    }).concat(this.checkNodes.map((checkNode, index) => {
+      checkNode.group = 'check';
+      checkNode.physics = false;
+      checkNode.x = index * TannerGraph.width / ( this.checkNodes.length + 1 );
+      checkNode.y = TannerGraph.height * 0.75;
+      return checkNode;
+    }))),
     edges: new TannerGraph.vis.DataSet(this.edges)
   };
   var options = {};
-  
+
   var network = new TannerGraph.vis.Network(container, data, options);
 };
 
-TannerGraph.prototype.renderSubGraph = function(subGraph){
-  
+TannerGraph.prototype.renderSubGraph = function(root){
+  if(!TannerGraph.vis) throw new Error("You need to define vis.js first");
+
+  var container = document.querySelector('#sub-graph');
+
+  var data = { nodes: [], edges: [] };
+  var queue = [root];
+  while(queue.length){
+    var node = queue.shift();
+    data.nodes.push(node);
+    node.children.forEach(function(child){
+      queue.push(child);
+      data.edges.push({
+        from: node.id,
+        to: child.id
+      });
+    });
+  }
+
+  var network = new TannerGraph.vis.Network(container, data, {
+    layout: { hierarchical: true }
+  });
 };
 
 TannerGraph.prototype.getNode = function(id){
@@ -45,25 +85,43 @@ TannerGraph.prototype.getNode = function(id){
 
 TannerGraph.prototype.subGraph = function(nodeId, depth){
   var rootNode = this.getNode(nodeId);
-    
-  var treeRoot = { 
+
+  var treeRoot = {
     ref: rootNode,
-    id: rootNode.id, 
-    label: rootNode.label, 
-    group: rootNode.group, 
-    children: [] 
+    id: rootNode.id,
+    label: rootNode.label,
+    group: rootNode.group,
+    children: [],
+    level: 0
   };
-  
-  var currentLevel = [treeRoot];
-  while(currentLevel.length && depth){
-    depth--;
-    var node = currentLevel.shift();
-    var childrenNodes = node.ref.neighbors
-      .map(id => this.getNode(id) )
-      .map(c => { return { ref: c, id: c.id, label: c.label, group: c.group, children: [] }; });
-    node.children = childrenNodes;
-    currentLevel = currentLevel.concat(childrenNodes);
+
+  var level = 0;
+  var usedNodes = [treeRoot];
+  var queue = [treeRoot];
+
+  while(queue.length && level < depth){
+    level++;
+    var levelQueue = [];
+    queue.forEach((node) => {
+      var childrenNodes = node.ref.connections
+      .map(connection => this.getNode(connection.id) )
+      .map(c => {
+        return {
+          ref: c,
+          id: c.id,
+          label: c.label,
+          level: level,
+          group: c.group,
+          children: []
+        };
+      });
+      node.children = childrenNodes.filter(function(child){
+        return usedNodes.every(usedNode => usedNode.id !== child.id);
+      });
+      usedNodes = usedNodes.concat(node.children);
+      levelQueue = levelQueue.concat(node.children);
+    });
+    queue = levelQueue;
   }
-  
   return treeRoot;
 };
